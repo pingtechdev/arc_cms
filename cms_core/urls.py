f7c59a5128f4261ad274367be7fd7378188dccd2
pingtechdev <pingtech.dev@gmail.com>
2025-10-20 19:02:33 +0300

    Fix URL configuration conflicts - remove duplicate API router registration

--- cms_core/urls.py
+++ cms_core/urls.py
@@ -9,19 +9,8 @@ from django.urls import path, include, re_path
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
-from wagtail.api.v2.router import WagtailAPIRouter
-from wagtail.api.v2.views import PagesAPIViewSet
-from wagtail.images.api.v2.views import ImagesAPIViewSet
-from wagtail.documents.api.v2.views import DocumentsAPIViewSet
-from cms_app.api import SiteSettingsAPIView
-
-# Create API router
-api_router = WagtailAPIRouter('wagtailapi')
-
-# Register API endpoints
-api_router.register_endpoint('pages', PagesAPIViewSet)
-api_router.register_endpoint('images', ImagesAPIViewSet)
-api_router.register_endpoint('documents', DocumentsAPIViewSet)
+# Import API router from cms_app
+from cms_app.api import api_router
 
 urlpatterns = [
     # Django Admin (for database management)
@@ -31,11 +20,7 @@ urlpatterns = [
     path('cms/', include(wagtailadmin_urls)),
     path('documents/', include(wagtaildocs_urls)),
 
-    # Wagtail API (for headless CMS)
-    path('api/v2/', api_router.urls),
-    
-    # Custom API endpoints
-    path('api/v2/settings/', SiteSettingsAPIView.as_view()),
+    # Wagtail API (for headless CMS) - handled by cms_app.urls
     
     # Custom app URLs
     path('', include('cms_app.urls')),
